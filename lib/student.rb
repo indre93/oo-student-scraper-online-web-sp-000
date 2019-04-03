90d7773655c997db7c752eec02e5eb28dc28577c
Indre Bakeer <indre.bakeer@gmail.com>
2019-04-03 19:07:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array = @@all
-    students_array
+    Student.all
   end
 
   def add_student_attributes(attributes_hash)
